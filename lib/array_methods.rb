77e5c74c3313cb7c55b9ea9074c21f22a5dd42c3
Di Zheng <dizhengcindy@gmail.com>
2020-02-08 20:33:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,7 +23,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-    minNum = 10000
+    minNum = find_max_value(array)
     counter = 0 
     while counter < array.length do
       if array[counter] < minNum
@@ -31,4 +31,5 @@ def find_min_value(array)
       end
       counter +=1 
     end
+    retun minNum
 end
